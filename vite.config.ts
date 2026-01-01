da9605c869145fc15faafe8389897d35b349df04
Marc Ford <mfdeux@users.noreply.github.com>
2026-01-01 17:50:41 -0600

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
+    port: 3000,
+    host: true,
     allowedHosts: [
       "localhost",
       ".ngrok.io",
