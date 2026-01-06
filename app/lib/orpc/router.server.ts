48cbcc6f2cdcbe3edc7c1ddd35a8bd9b208da83e
Marc Ford <marcrford@gmail.com>
2026-01-06 16:05:23 -0600

    Add usage tracking, guard on orpc endpoint

--- app/lib/orpc/router.server.ts
+++ app/lib/orpc/router.server.ts
@@ -9,6 +9,7 @@ import {
   getStripeSubscriptionSession,
 } from "~/lib/services/billing.server";
 
+import { addDays } from "date-fns";
 import prisma from "~/lib/utils/prisma.server";
 import { deleteAccount } from "../services/accounts.server";
 import { analyzeTestQuestionImage_Quick } from "../services/ai.server";
@@ -25,10 +26,59 @@ export const requiredAuthMiddleware = os
     });
   });
 
+const subscriptionCheck = os
+  .$context<{ account?: Account }>()
+  .middleware(async ({ context, next }) => {
+    const account = context.account;
+
+    if (!account) {
+      throw new ORPCError("UNAUTHORIZED");
+    }
+
+    const isActive =
+      account &&
+      account.subscriptionStatus === "active" &&
+      account.subscriptionPeriodEnd &&
+      account.subscriptionPeriodEnd > new Date();
+
+    if (isActive) {
+      const recentUsage = await prisma.tokenUsage.aggregate({
+        where: {
+          accountId: account.id,
+          createdAt: {
+            gte: addDays(new Date(), -14),
+          },
+        },
+        _sum: {
+          totalTokens: true,
+        },
+      });
+
+      const tokensUsed = recentUsage._sum.totalTokens || 0;
+
+      // If they have an active subscription but have used more than 100,000 tokens in the last week
+      if (tokensUsed >= 1_000_000) {
+        throw new ORPCError("UNAUTHORIZED");
+      }
+      return next({
+        context: { account },
+      });
+    }
+
+    if (account.messagesRemaining > 0) {
+      // Proceed to the next middleware or handler
+      return next({
+        context: { account },
+      });
+    }
+    throw new ORPCError("UNAUTHORIZED");
+  });
+
 export const router = {
   responses: {
     generateSolution: os
       .use(requiredAuthMiddleware)
+      .use(subscriptionCheck)
       .input(
         z.object({
           imageUrl: z.string(),
@@ -39,6 +89,25 @@ export const router = {
           await analyzeTestQuestionImage_Quick({
             imageUrl: input.imageUrl,
           });
+        await Promise.all([
+          prisma.tokenUsage.create({
+            data: {
+              accountId: context.account.id,
+              model: "google/gemini-2.5-flash",
+              inputTokens: usage.inputTokens,
+              outputTokens: usage.outputTokens,
+              totalTokens: usage.totalTokens,
+            },
+          }),
+          prisma.account.update({
+            where: { id: context.account.id },
+            data: {
+              messagesRemaining: {
+                decrement: 1,
+              },
+            },
+          }),
+        ]);
         return analysis;
       }),
   },
